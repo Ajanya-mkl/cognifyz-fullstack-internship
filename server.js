cf7c1e2630ea1c5a1078651a9c3b4964803f4c9b
Ajanya <mkl.ajanya@gmail.com>
2026-05-15 14:31:21 +0530

    Completed Cognifyz Task 2

--- server.js
+++ server.js
@@ -7,17 +7,26 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let users = [];
+
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.post("/submit", (req, res) => {
-    const { name, email, course } = req.body;
+    const { name, email, course, age } = req.body;
+
+    if (!name || !email || !course || !age) {
+        return res.send("All fields are required!");
+    }
+
+    users.push({ name, email, course, age });
 
     res.render("result", {
         name,
         email,
-        course
+        course,
+        age
     });
 });
 
